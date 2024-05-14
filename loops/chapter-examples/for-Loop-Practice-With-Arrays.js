706088152e52a6369fd6874ba00f32572e1f50b2
Aldi <aldialarcon02@gmail.com>
2024-05-13 20:17:18 -0500

    Practice with arrays

--- loops/chapter-examples/for-Loop-Practice-With-Arrays.js
+++ loops/chapter-examples/for-Loop-Practice-With-Arrays.js
@@ -1,3 +1,9 @@
 // create an array variable containing the names
+let familyMembers = ["Flavia", "Aldi","Cesar","Cesarito","Kevin"]
+
 
 // write a for loop that prints each name on a different line
+
+for (let i = 0; i < familyMembers.length; i++) {
+    console.log(familyMembers[i]);
+}
