565f9605827fb1fe5a7d7e9c54655ebb0fce73e2
Aldi <aldialarcon02@gmail.com>
2024-05-16 12:07:54 -0500

    for Loops

--- loops/exercises/for-Loop-Exercises.js
+++ loops/exercises/for-Loop-Exercises.js
@@ -1,14 +1,33 @@
 /*Exercise #1: Construct for loops that accomplish the following tasks:
     a. Print the numbers 0 - 20, one number per line.
+
+
     b. Print only the ODD values from 3 - 29, one number per line.
     c. Print the EVEN numbers 12 to -14 in descending order, one number per line.
     d. Challenge - Print the numbers 50 - 20 in descending order, but only if the numbers are multiples of 3. (Your code should work even if you replace 50 or 20 with other numbers). */
 
+    for (let i = 0; i < 21; i++) {
+      console.log(i)
+    }
+
+for (let i = -14; i < 12; i = i + 3) {
+  console.log(i);
+
+}
 
 
+let words = ['1','5','LC101','blue','42']
 
+for (let i=0; i<words.length; i++) {
+  console.log(words[i])
+}
 /*Exercise #2: 
 Initialize two variables to hold the string “LaunchCode” and the array [1, 5, ‘LC101’, ‘blue’, 42].
+
+
+
+
+
 
 
 Construct ``for`` loops to accomplish the following tasks:
