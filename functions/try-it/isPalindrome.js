6c3c48cf548dd1008fbcbd26a20a8fcdd69f8715
Aldi <aldialarcon02@gmail.com>
2024-05-23 08:44:27 -0500

    Functions

--- functions/try-it/isPalindrome.js
+++ functions/try-it/isPalindrome.js
@@ -1,7 +1,50 @@
-function reverse(str) {
-   return str.split('').reverse().join('');
+// function reverse(str) {
+//    return str.split('').reverse().join('');
+// }
+
+// function isPalindrome(str) {
+//    return reverse(str) === str;
+// }
+
+
+// function sayHello() {
+//    console.log("Hello, World!");
+// }
+
+// sayHello();
+
+// function sumToN(n) {
+//    let sum = 0;
+//    for (let i = 0; i <= n; i++) {
+//        sum += i;
+//    }
+//    return sum;
+// }
+
+// console.log(sumToN(3));
+
+// function plusTwo(num) {
+//    return num + 2;
+// }
+
+// let a = 2;
+
+// for (let i=0; i < 4; i++) {
+//    a = plusTwo(a);
+// }
+
+// console.log(a);
+
+// function repeater(str) {
+//    let repeated = str + str;
+//    console.log(repeated);
+// }
+
+// repeater('Bob');
+
+function repeater(str) {
+   let repeated = str + str;
+   console.log(repeated);
 }
 
-function isPalindrome(str) {
-   return reverse(str) === str;
-}
+repeater('Bob');
--- functions/try-it/sayHello.js
+++ functions/try-it/sayHello.js
@@ -1,3 +1,16 @@
 function sayHello() {
    console.log("Hello, World!");
 }
+
+
+
+// let num = 42;
+
+// function isEven (num) { 
+//     return num % 2 === 0; 
+// }
+
+// console.log(isEven(43));
+
+
+
